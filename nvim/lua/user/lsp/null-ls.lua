d7cd566749106308318532997b4a5b33f1025eb9
NoamMuallem <noammu072@gmail.com>
2022-01-25 22:19:53 +0200

    make lsp and formatting better

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -10,10 +10,13 @@ local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    -- diagnostics.flake8
-	},
+  sources={
+	formatting.prettierd,
+  formatting.stylua,
+  formatting.fish_indent,
+  formatting.fixjson.with({ filetypes = { "jsonc" } }),
+  diagnostics.shellcheck,
+  diagnostics.markdownlint,
+  diagnostics.selene,
+  }
 })
