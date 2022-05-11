f382e65e8b06c3ff137c9378d4d8356a68ec1051
Noam Muallem <noammu072@gmail.com>
2022-05-11 11:48:47 +0300

    file path in galaxy line start from root folder and not system root

--- nvim/lua/user/galaxyline.lua
+++ nvim/lua/user/galaxyline.lua
@@ -4,63 +4,62 @@ local gls = gl.section
 local extension = require('galaxyline.provider_extensions')
 
 gl.short_line_list = {
-    'LuaTree',
-    'NvimTree',
-    'vista',
-    'dbui',
-    'startify',
-    'term',
-    'nerdtree',
-    'fugitive',
-    'fugitiveblame',
-    'plug',
-    'plugins'
+  'LuaTree',
+  'NvimTree',
+  'vista',
+  'dbui',
+  'startify',
+  'term',
+  'nerdtree',
+  'fugitive',
+  'fugitiveblame',
+  'plug',
+  'plugins'
 }
 
 VistaPlugin = extension.vista_nearest
 
-local ProgFileTypes = {'lua', 'python', 'typescript', 'typescriptreact', 'react', 'javascript', 'javascriptreact', 'rust', 'go', 'html'}
+local ProgFileTypes = { 'lua', 'python', 'typescript', 'typescriptreact', 'react', 'javascript', 'javascriptreact', 'rust', 'go', 'html' }
 
 -- for checking value in table
-local function has_value (tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
-        end
+local function has_value(tab, val)
+  for index, value in ipairs(tab) do
+    if value == val then
+      return true
     end
+  end
 
-    return false
+  return false
 end
 
-
 local function get_color()
-    if vim.o.background == 'light' then
-        return "#fff"
-    end
-    return "#353644"
+  if vim.o.background == 'light' then
+    return "#fff"
+  end
+  return "#353644"
 end
 
 local function get_color_bg()
-    if vim.o.background == 'light' then
-        return "#fff"
-    end
-    return "#282c34"
+  if vim.o.background == 'light' then
+    return "#fff"
+  end
+  return "#282c34"
 end
 
 -- then change in colors line_bg = get_color_bg
 
 local colors = {
 
-    bg = "#33373E",
-    line_bg = "#3D4147",
-    fg = '#A3ABB8',
-    fg_green = '#65a380',
+  bg       = "#33373E",
+  line_bg  = "#3D4147",
+  fg       = '#A3ABB8',
+  fg_green = '#65a380',
   yellow   = '#E5C07B',
   cyan     = '#008080',
   darkblue = '#081633',
   green    = '#98be65',
   orange   = '#FF8800',
-  purple = '#5d4d7a',
+  purple   = '#5d4d7a',
   magenta  = '#c678dd',
   blue     = '#51afef',
   red      = '#ec5f67',
@@ -77,10 +76,12 @@ local function file_readonly(readonly_icon)
   end
   return ''
 end
+
 -- get current file name
 local function get_current_file_name()
   -- local file = vim.fn.expand('%:t')
-  local file = vim.fn.expand('%:f')
+  -- local file = vim.fn.expand('%:f')
+  local file = string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd(), '')
   if vim.fn.empty(file) == 1 then return '' end
   if string.len(file_readonly(readonly_icon)) ~= 0 then
     return file .. file_readonly(readonly_icon)
@@ -94,23 +95,21 @@ local function get_current_file_name()
   return file .. ' '
 end
 
-
 local function lsp_status(status)
-    shorter_stat = ''
-    for match in string.gmatch(status, "[^%s]+")  do
-        err_warn = string.find(match, "^[WE]%d+", 0)
-        if not err_warn then
-            shorter_stat = shorter_stat .. ' ' .. match
-        end
+  shorter_stat = ''
+  for match in string.gmatch(status, "[^%s]+") do
+    err_warn = string.find(match, "^[WE]%d+", 0)
+    if not err_warn then
+      shorter_stat = shorter_stat .. ' ' .. match
     end
-    return shorter_stat
+  end
+  return shorter_stat
 end
-
 
 local function get_coc_lsp()
   local status = vim.fn['coc#status']()
   if not status or status == '' then
-      return ''
+    return ''
   end
   return lsp_status(status)
 end
@@ -118,51 +117,51 @@ end
 function get_diagnostic_info()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_lsp()
-    end
+  end
   return ''
 end
 
 local function get_current_func()
-  local has_func, func_name = pcall(vim.api.nvim_buf_get_var,0,'coc_current_function')
+  local has_func, func_name = pcall(vim.api.nvim_buf_get_var, 0, 'coc_current_function')
   if not has_func then return end
-      return func_name
-  end
+  return func_name
+end
 
 function get_function_info()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_current_func()
-    end
+  end
   return ''
 end
 
 local function trailing_whitespace()
-    local trail = vim.fn.search("\\s$", "nw")
-    if trail ~= 0 then
-        return ' '
-    else
-        return nil
-    end
+  local trail = vim.fn.search("\\s$", "nw")
+  if trail ~= 0 then
+    return ' '
+  else
+    return nil
+  end
 end
 
 -- current_func_status with treesitter
 local function get_current_func_from_treesitter()
   local opts = {
     indicator_size = 100,
-    type_patterns = {'class', 'function', 'method', },
+    type_patterns = { 'class', 'function', 'method', },
     transform_fn = function(line) return line:gsub('%s*[%[%(%{]*%s*$', '') end,
     separator = '  ',
   }
   local status = vim.fn['nvim_treesitter#statusline'](opts)
   if not status or status == '' then
-      return ''
+    return ''
   end
-      return status
-  end
+  return status
+end
 
 local function get_current_context()
   if vim.fn.exists('nvim_treesitter#statusline') == 1 then
     return get_current_func_from_treesitter()
-    end
+  end
   return ''
 end
 
@@ -172,24 +171,23 @@ TreesitterContext = get_current_func_from_treesitter
 TrailingWhiteSpace = trailing_whitespace
 
 function has_file_type()
-    local f_type = vim.bo.filetype
-    if not f_type or f_type == '' then
-        return false
-    end
-    return true
+  local f_type = vim.bo.filetype
+  if not f_type or f_type == '' then
+    return false
+  end
+  return true
 end
 
 function has_file_prog_filetype()
-    local f_type = vim.bo.filetype
-    if not f_type or f_type == '' then
-        return false
-    end
-    if has_value(ProgFileTypes, f_type) then
-        return true
-    end
+  local f_type = vim.bo.filetype
+  if not f_type or f_type == '' then
     return false
+  end
+  if has_value(ProgFileTypes, f_type) then
+    return true
+  end
+  return false
 end
-
 
 local buffer_not_empty = function()
   if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
@@ -201,21 +199,21 @@ end
 gls.left[1] = {
   FirstElement = {
     provider = function() return ' ' end,
-    highlight = {colors.blue,colors.line_bg}
+    highlight = { colors.blue, colors.line_bg }
   },
 }
-gls.left[3] ={
+gls.left[3] = {
   FileIcon = {
     provider = 'FileIcon',
     condition = buffer_not_empty,
-    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.line_bg},
+    highlight = { require('galaxyline.provider_fileinfo').get_file_icon_color, colors.line_bg },
   },
 }
 gls.left[4] = {
   FileName = {
-    provider = {get_current_file_name},
+    provider = { get_current_file_name },
     condition = buffer_not_empty,
-    highlight = {colors.fg,colors.line_bg,'bold'}
+    highlight = { colors.fg, colors.line_bg, 'bold' }
   }
 }
 
@@ -223,19 +221,19 @@ gls.left[5] = {
   GitIcon = {
     provider = function() return '  ' end,
     condition = require('galaxyline.provider_vcs').check_git_workspace,
-    highlight = {colors.yellow,colors.line_bg},
+    highlight = { colors.yellow, colors.line_bg },
   }
 }
 gls.left[6] = {
   GitBranch = {
     provider = 'GitBranch',
     condition = require('galaxyline.provider_vcs').check_git_workspace,
-    highlight = {colors.yellow,colors.line_bg,'bold'},
+    highlight = { colors.yellow, colors.line_bg, 'bold' },
   }
 }
 
 local checkwidth = function()
-  local squeeze_width  = vim.fn.winwidth(0) / 2
+  local squeeze_width = vim.fn.winwidth(0) / 2
   if squeeze_width > 40 then
     return true
   end
@@ -243,7 +241,7 @@ local checkwidth = function()
 end
 gls.left[13] = {
   Space = {
-    provider = function () return '' end
+    provider = function() return '' end
   }
 }
 
@@ -251,75 +249,75 @@ gls.left[10] = {
   LeftEnd = {
     provider = function() return '' end,
     separator = '',
-    separator_highlight = {colors.bg,colors.line_bg},
-    highlight = {colors.line_bg,colors.line_bg}
+    separator_highlight = { colors.bg, colors.line_bg },
+    highlight = { colors.line_bg, colors.line_bg }
   }
 }
 
 gls.left[11] = {
-    TrailingWhiteSpace = {
-     provider = TrailingWhiteSpace,
-     icon = '   ',
-     highlight = {colors.yellow,colors.bg},
-    }
+  TrailingWhiteSpace = {
+    provider = TrailingWhiteSpace,
+    icon = '   ',
+    highlight = { colors.yellow, colors.bg },
+  }
 }
 
 gls.left[12] = {
   DiagnosticError = {
     provider = 'DiagnosticError',
     icon = '  ',
-    highlight = {colors.red,colors.bg}
+    highlight = { colors.red, colors.bg }
   }
 }
 gls.left[13] = {
   Space = {
-    provider = function () return '' end
+    provider = function() return '' end
   }
 }
 gls.left[14] = {
   DiagnosticWarn = {
     provider = 'DiagnosticWarn',
     icon = '   ',
-    highlight = {colors.yellow,colors.bg},
+    highlight = { colors.yellow, colors.bg },
   }
 }
- gls.left[16] = {
-   CocFunc = {
-     provider = CocFunc,
-     icon = ' ',
-     highlight = {colors.yellow,colors.bg},
-   }
- }
-
- gls.left[17] = {
-   TreesitterContext = {
-     provider = TreesitterContext,
-     condition = has_file_prog_filetype,
-     icon = '  λ ',
-     highlight = {colors.yellow,colors.bg},
-   }
- }
+gls.left[16] = {
+  CocFunc = {
+    provider = CocFunc,
+    icon = ' ',
+    highlight = { colors.yellow, colors.bg },
+  }
+}
 
 gls.left[17] = {
-nvimGPS = {
-        provider = function()
-            return gps.get_location()
-        end,
-        condition = function()
-            return gps.is_available()
-        end,
-        icon = '  ',
-        highlight = {colors.yellow,colors.bg},
-    }
+  TreesitterContext = {
+    provider = TreesitterContext,
+    condition = has_file_prog_filetype,
+    icon = '  λ ',
+    highlight = { colors.yellow, colors.bg },
+  }
+}
+
+gls.left[17] = {
+  nvimGPS = {
+    provider = function()
+      return gps.get_location()
+    end,
+    condition = function()
+      return gps.is_available()
+    end,
+    icon = '  ',
+    highlight = { colors.yellow, colors.bg },
+  }
 }
 
 gls.short_line_right[1] = {
   BufferIcon = {
-    provider= 'BufferIcon',
+    provider = 'BufferIcon',
     separator = '',
     condition = has_file_type,
-    separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.fg,colors.purple}
+    separator_highlight = { colors.purple, colors.bg },
+    highlight = { colors.fg, colors.purple }
   }
 }
 
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -272,7 +272,7 @@ _G.packer_plugins = {
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
-    after = { "cmp-path", "cmp-cmdline", "cmp-buffer", "LuaSnip", "cmp_luasnip", "friendly-snippets", "cmp-nvim-lsp", "nvim-autopairs" },
+    after = { "cmp-buffer", "cmp-cmdline", "LuaSnip", "cmp-path", "friendly-snippets", "nvim-autopairs", "cmp-nvim-lsp", "cmp_luasnip" },
     config = { "\27LJ\2\n(\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\ruser.cmp\frequire\0" },
     loaded = false,
     needs_bufread = false,
@@ -308,7 +308,7 @@ _G.packer_plugins = {
     url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
-    after = { "nvim-lsp-installer", "null-ls.nvim", "nlsp-settings.nvim" },
+    after = { "nlsp-settings.nvim", "null-ls.nvim", "nvim-lsp-installer" },
     config = { "\27LJ\2\n-\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\18user.lsp.init\frequire\0" },
     loaded = false,
     needs_bufread = false,
@@ -389,7 +389,7 @@ _G.packer_plugins = {
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["trouble.nvim"] = {
-    config = { "\27LJ\2\n]\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\14auto_open\1\25use_diagnostic_signs\2\nsetup\ftrouble\frequire\0" },
+    config = { "\27LJ\2\n]\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\25use_diagnostic_signs\2\14auto_open\1\nsetup\ftrouble\frequire\0" },
     loaded = true,
     path = "/Users/noammuallem/.local/share/nvim/site/pack/packer/start/trouble.nvim",
     url = "https://github.com/folke/trouble.nvim",
@@ -446,37 +446,37 @@ if not vim.g.packer_custom_loader_enabled then
   vim.g.packer_custom_loader_enabled = true
 end
 
--- Config for: git-conflict.nvim
-time([[Config for git-conflict.nvim]], true)
-try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\17git-conflict\frequire\0", "config", "git-conflict.nvim")
-time([[Config for git-conflict.nvim]], false)
--- Config for: galaxyline.nvim
-time([[Config for galaxyline.nvim]], true)
-try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20user.galaxyline\frequire\0", "config", "galaxyline.nvim")
-time([[Config for galaxyline.nvim]], false)
--- Config for: telescope.nvim
-time([[Config for telescope.nvim]], true)
-try_loadstring("\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19user.telescope\frequire\0", "config", "telescope.nvim")
-time([[Config for telescope.nvim]], false)
 -- Config for: dim.lua
 time([[Config for dim.lua]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\bdim\frequire\0", "config", "dim.lua")
 time([[Config for dim.lua]], false)
+-- Config for: galaxyline.nvim
+time([[Config for galaxyline.nvim]], true)
+try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20user.galaxyline\frequire\0", "config", "galaxyline.nvim")
+time([[Config for galaxyline.nvim]], false)
 -- Config for: trouble.nvim
 time([[Config for trouble.nvim]], true)
-try_loadstring("\27LJ\2\n]\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\14auto_open\1\25use_diagnostic_signs\2\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+try_loadstring("\27LJ\2\n]\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\25use_diagnostic_signs\2\14auto_open\1\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
 time([[Config for trouble.nvim]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19user.telescope\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+-- Config for: git-conflict.nvim
+time([[Config for git-conflict.nvim]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\17git-conflict\frequire\0", "config", "git-conflict.nvim")
+time([[Config for git-conflict.nvim]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewToggleFiles lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewToggleFiles", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewOpen lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewOpen", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeToggle lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeClose lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeClose", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewFocusFiles lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewFocusFiles", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewClose lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewClose", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Neogit lua require("packer.load")({'neogit'}, { cmd = "Neogit", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewToggleFiles lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewToggleFiles", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file HopWord lua require("packer.load")({'hop.nvim'}, { cmd = "HopWord", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeClose lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeClose", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Neogit lua require("packer.load")({'neogit'}, { cmd = "Neogit", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file NvimTreeToggle lua require("packer.load")({'nvim-tree.lua'}, { cmd = "NvimTreeToggle", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file DiffviewFocusFiles lua require("packer.load")({'diffview.nvim'}, { cmd = "DiffviewFocusFiles", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 time([[Defining lazy-load commands]], false)
 
 -- Keymap lazy-loads
@@ -488,10 +488,10 @@ vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
+vim.cmd [[au BufReadPre * ++once lua require("packer.load")({'gitsigns.nvim', 'nvim-colorizer.lua', 'indent-blankline.nvim', 'luatab.nvim', 'nvim-lspconfig', 'nvim-scrollbar', 'dressing.nvim'}, { event = "BufReadPre *" }, _G.packer_plugins)]]
 vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'nvim-cmp'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
-vim.cmd [[au CursorMoved * ++once lua require("packer.load")({'vim-matchup'}, { event = "CursorMoved *" }, _G.packer_plugins)]]
 vim.cmd [[au BufRead * ++once lua require("packer.load")({'nvim-treesitter'}, { event = "BufRead *" }, _G.packer_plugins)]]
-vim.cmd [[au BufReadPre * ++once lua require("packer.load")({'nvim-colorizer.lua', 'luatab.nvim', 'indent-blankline.nvim', 'gitsigns.nvim', 'nvim-scrollbar', 'dressing.nvim', 'nvim-lspconfig'}, { event = "BufReadPre *" }, _G.packer_plugins)]]
+vim.cmd [[au CursorMoved * ++once lua require("packer.load")({'vim-matchup'}, { event = "CursorMoved *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 if should_profile then save_profiles() end
